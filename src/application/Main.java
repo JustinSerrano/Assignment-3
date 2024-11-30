ad2fdf07270dca20c78b5b70b2e9828752c63a70
Justin Serrano <60021371+JustinSerrano@users.noreply.github.com>
2024-11-29 22:22:56 -0700

    Search UI
    - Started the Search UI
    - Updated FXML
    - Updated MainViewController

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,34 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
-
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("../view/MainView.fxml"));
-			Scene scene = new Scene(root,400,400);
+			// Load the main FXML layout
+			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("../view/MainView.fxml"));
+
+			// Create a scene with the desired size
+			Scene scene = new Scene(root, 800, 600);
+
+			// Optional if CSS styling in separate file
 			scene.getStylesheets().add(getClass().getResource("../view/application.css").toExternalForm());
+
+			// Set the stage properties
+			primaryStage.setTitle("Toy Store Company");
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(true);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/controller/MainViewController.java
+++ src/controller/MainViewController.java
@@ -1,5 +1,34 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
 public class MainViewController {
-	
+
+	@FXML
+	private Button btnSearch, btnClear, btnBuy;
+
+	@FXML
+	private TextField inputSerialNumber, inputName, inputType;
+
+	@FXML
+	private Label lblSerialNumber, lblName, lblType, lblResult;
+
+	@FXML
+	private RadioButton rbSerialNumber, rbName, rbType;
+
+	@FXML
+	void BuyListener(ActionEvent event) {
+
+	}
+
+	@FXML
+	void searchListener(ActionEvent event) {
+
+	}
+
 }
